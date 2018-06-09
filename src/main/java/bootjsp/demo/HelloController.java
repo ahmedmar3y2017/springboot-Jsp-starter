1701ad4b32344ea74f9f39775f032c732f61e18e
ahmed mar3y <ahmedmar3y108@gmail.com>
2018-06-09 13:17:03 +0200

    Initial commit

---
+++ src/main/java/bootjsp/demo/HelloController.java
@@ -0,0 +1,20 @@
+package bootjsp.demo;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+
+
+    @RequestMapping("/home")
+    public String getMain() {
+
+        System.out.println("Done");
+        return "index.jsp";
+
+    }
+
+
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,11 @@
+<html>
+
+<body>
+
+
+ahmedmohamed
+
+</body>
+
+
+</html>
